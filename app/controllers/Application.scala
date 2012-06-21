468e444fba8746a7e9b9a64d26cffaab2085ad89
ubourdon <bourdon.ugo@gmail.com>
2012-06-21 11:48:57 +0200

    add: LogBuilder

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,27 +1,28 @@
 package controllers
 
 import play.api.mvc._
-import models.Log
 import services.LogParser
 import log.pattern.LogPattern
+import models.{LogBuilder, Log}
 
 object Application extends Controller {
 
     def index = Action {
         val parser = new LogParser( "public/karaf.log" )
 
-        val log: Log = Log( parser.all,
-                            parser.retrieveValueFromPattern( LogPattern.DATE ).getOrElse( "none" ),
-                            parser.retrieveValueFromPattern( LogPattern.BATCH_DURATION ).getOrElse( "none" ),
-                            parser.retrieveValueFromPattern( LogPattern.RETRIEVE_EVENTS ).getOrElse( "none" ),
-                            parser.retrieveValueFromPattern( LogPattern.CALLBACK_EVENTS ).getOrElse( "none" ),
-                            parser.retrieveValueFromPattern( LogPattern.ACCOUNT_UPLOAD ).getOrElse( "none" ),
-                            parser.retrieveValueFromPattern( LogPattern.CONTACT_UPLOAD ).getOrElse( "none" ),
-                            parser.retrieveValueFromPattern( LogPattern.OPPORTUNITY_UPLOAD ).getOrElse( "none" ),
-                            parser.retrieveValueFromPattern( LogPattern.ACCOUNT_DOWNLOAD ).getOrElse( "none" ),
-                            parser.retrieveValueFromPattern( LogPattern.CONTACT_DOWNLOAD ).getOrElse( "none" ),
-                            parser.retrieveValueFromPattern( LogPattern.OPPORTUNITY_DOWNLOAD ).getOrElse( "none" )
-        )
+        val log: Log = new LogBuilder()
+            .withEntireContent( parser.prettyAll )
+            .withDate( parser.retrieveValueFromPattern( LogPattern.DATE ).getOrElse( "none" ) )
+            .withDuration( parser.retrieveValueFromPattern( LogPattern.BATCH_DURATION ).getOrElse( "none" ) )
+            .withRetrieve( parser.retrieveValueFromPattern( LogPattern.RETRIEVE_EVENTS ).getOrElse( "none" ) )
+            .withCallback( parser.retrieveValueFromPattern( LogPattern.CALLBACK_EVENTS ).getOrElse( "none" ) )
+            .withAccountUploaded( parser.retrieveValueFromPattern( LogPattern.ACCOUNT_UPLOAD ).getOrElse( "none" ) )
+            .withContactUploaded( parser.retrieveValueFromPattern( LogPattern.CONTACT_UPLOAD ).getOrElse( "none" ) )
+            .withOpportunityUploaded( parser.retrieveValueFromPattern( LogPattern.OPPORTUNITY_UPLOAD ).getOrElse( "none" ) )
+            .withAccountDownloaded( parser.retrieveValueFromPattern( LogPattern.ACCOUNT_DOWNLOAD ).getOrElse( "none" ) )
+            .withContactDownloaded( parser.retrieveValueFromPattern( LogPattern.CONTACT_DOWNLOAD ).getOrElse( "none" ) )
+            .withOpportunityDownloaded( parser.retrieveValueFromPattern( LogPattern.OPPORTUNITY_DOWNLOAD ).getOrElse( "none" ) )
+            .toLog
 
         Ok( views.html.loaderEvents( "batch loader du 25 mai 2012", log ) )
     }
--- app/models/Log.scala
+++ app/models/Log.scala
@@ -14,3 +14,75 @@ case class Log( entireContent: Html,
                 contactDownloaded: String,
                 opportunityDownloaded: String
 )
+
+class LogBuilder {
+
+    var entireContent: Html = _
+    var date: String = _
+    var duration: String = _
+    var retrieve: String = _
+    var callback: String = _
+    var accountUploaded: String = _
+    var contactUploaded: String = _
+    var opportunityUploaded: String = _
+    var accountDownloaded: String = _
+    var contactDownloaded: String = _
+    var opportunityDownloaded: String = _
+
+    def toLog: Log = Log( entireContent, date, duration, retrieve, callback, accountUploaded, contactUploaded, opportunityUploaded, accountDownloaded, contactDownloaded, opportunityDownloaded )
+    
+    def withEntireContent( entireContent: Html ): LogBuilder = {
+        this.entireContent = entireContent
+        this
+    }
+
+    def withDate( date: String ): LogBuilder = {
+        this.date = date
+        this
+    }
+
+    def withDuration( duration: String ): LogBuilder = {
+        this.duration = duration
+        this
+    }
+
+    def withRetrieve( retrieve: String ): LogBuilder = {
+        this.retrieve = retrieve
+        this
+    }
+
+    def withCallback( callback: String ): LogBuilder = {
+        this.callback = callback
+        this
+    }
+
+    def withAccountUploaded( accountUploaded: String ): LogBuilder = {
+        this.accountUploaded = accountUploaded
+        this
+    }
+
+    def withContactUploaded( contactUploaded: String ): LogBuilder = {
+        this.contactUploaded = contactUploaded
+        this
+    }
+
+    def withOpportunityDownloaded( opportunityUploaded: String ): LogBuilder = {
+        this.opportunityUploaded = opportunityUploaded
+        this
+    }
+
+    def withAccountDownloaded( accountDownloaded: String ): LogBuilder = {
+        this.accountDownloaded = accountDownloaded
+        this
+    }
+
+    def withContactDownloaded( contactDownloaded: String ): LogBuilder = {
+        this.contactDownloaded = contactDownloaded
+        this
+    }
+
+    def withOpportunityUploaded( opportunityDownloaded: String ): LogBuilder = {
+        this.opportunityDownloaded = opportunityDownloaded
+        this
+    }
+}
